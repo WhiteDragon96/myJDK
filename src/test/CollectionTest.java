0200ec325e5aa772b3f477c4ec7fd8061bc666a5
tangcs <tangcs@neusensetech.com>
2022-06-29 17:13:08 +0800

    init

---
+++ src/test/CollectionTest.java
@@ -0,0 +1,14 @@
+package src.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author tcs
+ */
+public class CollectionTest {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        System.out.println(list);
+    }
+}
